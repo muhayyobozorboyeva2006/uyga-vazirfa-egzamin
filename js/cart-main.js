f3c23b7dd6b705251fdf8088d0c9ebae5a95a3db
muxayyobozorboyeva1980 <muxayobozorboyeva6@gmali.com>
2025-11-03 19:20:47 +0500

    uygavazifa

--- js/cart-main.js
+++ js/cart-main.js
@@ -10,36 +10,36 @@ badge.textContent = diCartscart.length
 
 function showcards(content, data) {
     content.innerHTML = "";
-    data.map((product) => {
+    data.map((el) => {
         content.innerHTML += `
               <div class="aksiy2">
               <div class="h-[100px]">
-                  <img class="aksiy2-img" src="${product.images[0]}" alt="${product.name}">
-                  <p class="aksiyo-p">${product.discount}%</p>
+                  <img class="aksiy2-img" src="${el.images[0]}" alt="${el.name}">
+                  <p class="aksiyo-p">${el.discount}%</p>
                 <img class="like-img" src="../paka/Button (4).png" alt="layk">
               </div>
   
               <div class="aksiy3">
                 <div class="aksiy4">
-                  <h1 class="aksiy4-h1">${product.price} ₽</h1>
+                  <h1 class="aksiy4-h1">${el.price} ₽</h1>
                   <h2 class="aksiy4-h2">С картой</h2>
                 </div>
                 <div class="aksiy4">
-                  <h1 class="aksiy4-h1">${product.price - 10} ₽</h1>
+                  <h1 class="aksiy4-h1">${el.price - 10} ₽</h1>
                   <h2 class="aksiy4-h2">Обычная</h2>
                 </div>
              </div>
-              <p class="aksiy1-p">${product.description}</p>
+              <p class="aksiy1-p">${el.description}</p>
               <img class="aksiy1-img1" src="../assets/images/general/rating (1).png" alt="yulduzcha">
-              ${diCartscart.find((el) => el.id === product.id) ?
-                `  <div class="buttoncard">
-              <button onClick="decreaseQuatity(${product.id})" class="btncard1">-</button>
-              <span>${diCartscart.find((el) => el.id === product.id).qty}</span>
-              <button onClick="increaseQuatity(${product.id})" class="btncard2">+</button>
-              </div>` :
-                `<button onClick="addToCart(${product.id})"
-                class="aksiy1-button" >В корзину</button>`
-            }
+          
+                  <div class="buttoncard">
+              <button onClick="decreaseQuatity(${el.id})" class="btncard1">-</button>
+              <span>${el.qty}</span>
+              <button onClick="increaseQuatity(${el.id})" class="btncard2">+</button>
+              </div>:
+           
+            <button onClick="addToCart(${el.id})"
+              class="aksiy1-button" >В корзину</button>
                </div>
       `
     })
